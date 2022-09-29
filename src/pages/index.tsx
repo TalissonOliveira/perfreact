183b6ac2c331b421684c6c07e9aa99e760d336b9
Talisson Oliveira <talissonlux@outlook.com>
2022-09-29 01:07:56 -0300

    feat: adds formatting and calculation of data at search time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,24 @@ import type { NextPage } from 'next'
 import { FormEvent, useCallback, useState } from 'react'
 import { SearchResults } from '../components/SearchResults'
 
+interface Product {
+  id: number
+  price: number
+  priceFormatted: string
+  title: string
+}
+
+interface Results {
+  totalPrice: number
+  data: Product[]
+}
+
 const Home: NextPage = () => {
   const [search, setSearch] = useState('')
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState<Results>({
+    totalPrice: 0,
+    data: []
+  })
 
   async function handleSearch(event: FormEvent) {
     event.preventDefault()
@@ -16,7 +31,25 @@ const Home: NextPage = () => {
     const response = await fetch(`http://localhost:3333/products?q=${search}`)
     const data = await response.json()
 
-    setResults(data)
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    const products = data.map((product: Product) => {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        priceFormatted: formatter.format(product.price)
+      }
+    })
+
+    const totalPrice = data.reduce((total: number, product: Product) => {
+      return total + product.price
+    }, 0)
+
+    setResults({ totalPrice, data: products })
   }
 
    const addToWishList = useCallback(async (id: number) => {
@@ -37,7 +70,8 @@ const Home: NextPage = () => {
       </form>
 
       <SearchResults
-        results={results}
+        results={results.data}
+        totalPrice={results.totalPrice}
         onAddToWishList={addToWishList}
       />
     </div>

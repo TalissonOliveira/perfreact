cd483af58906c07cd3b44db8a33df82797bd3990
Talisson Oliveira <talissonlux@outlook.com>
2022-10-02 04:35:40 -0300

    feat: adds virtualization to render only items that are on screen

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from 'react-virtualized'
 import { ProductItem } from './ProductItem'
 
 interface SearchResultsProps {
@@ -12,16 +13,28 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, totalPrice, onAddToWishList }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => (
-        <ProductItem
-          key={product.id}
-          product={product}
-          onAddToWishList={onAddToWishList}
-        />
-      ))}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
